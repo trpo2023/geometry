e29a52b8e5c7cbb158d09827c58c7db50e199ae2
root <root@LAPTOP-6QUNCNHT>
2023-04-05 12:02:50 +0700

    switch

--- src/libgeometry/geometry.c
+++ src/libgeometry/geometry.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
-#define M_PI 3.14159265358979323846
+#include <math.h>
+
 #include "geometry.h"
 
 void circle(float R)
@@ -25,41 +26,60 @@ int degree(int x, int y)
     return result;
 }
 
-bool test(char* arr, int size)
+bool check_syntax(char* arr)
 {
-    int counter = 0, counter_2 = 0;
-    int test = 0, test_3 = 0;
-
-    for (int n = 0; n < size / 2; n++) {
-        if (arr[n] == ')') {
-            counter++;
-            test = n + 1;
-        }
-    }
-
-    for (int n = size / 2; n < size; n++) {
-        if (arr[n] == '(') {
-            counter_2++;
-            test_3 = n + 1;
-        }
-    }
-
     if (arr[0] != 'c' || arr[1] != 'i' || arr[2] != 'r' || arr[3] != 'c'
         || arr[4] != 'l' || arr[5] != 'e'
         || (arr[6] == ' ' && ((int)arr[7] > 41 || (int)arr[7] < 40))
         || ((int)arr[6] > 41 || (int)arr[6] < 40)) {
         printf("Error at column 0: expected 'circle'\n");
         return false;
-    } else if (counter > 0) {
-        printf("Error at column %d: expected ')'\n", test);
+    } 
+    return true;
+}
+
+bool check_syntax_2(char* arr,int size)
+{
+    if(check_syntax(arr) == true){
+        int counter = 0, counter_2 = 0;
+        int test = 0, test_3 = 0;
+
+        for (int n = 0; n < size / 2; n++) {
+            if (arr[n] == ')') {
+                counter++;
+                test = n + 1;
+            }
+        }
+
+        for (int n = size / 2; n < size; n++) {
+            if (arr[n] == '(') {
+                counter_2++;
+                test_3 = n + 1;
+            }
+        }
+
+        if (counter > 0) {
+            printf("Error at column %d: expected ')'\n", test);
+            return false;
+        } else if (counter_2 > 0) {
+            printf("Error at column %d: expected '('\n", test_3);
+            return false;
+        }
+    }else{
         return false;
-    } else if (counter_2 > 0) {
-        printf("Error at column %d: expected '('\n", test_3);
-        return false;
-    } else if (
-            arr[size - 1] != ')'
-            || (arr[size - 2] == ')' && arr[size - 1] != ' ')) {
-        printf("Error at column %d: unexpected token\n", size);
+    }
+
+    return true;
+}
+
+bool check_syntax_3(char *arr, int size)
+{
+    if(check_syntax_2(arr,size) == true){
+        if(arr[size - 1] != ')' || (arr[size - 2] == ')' && arr[size - 1] != ' ')) {
+            printf("Error at column %d: unexpected token\n", size);
+            return false;
+        }
+    }else{
         return false;
     }
     return true;
--- src/libgeometry/geometry.h
+++ src/libgeometry/geometry.h
@@ -10,6 +10,10 @@ void circle(float R);
 
 int degree(int x, int y);
 
-bool test(char* arr, int size);
+bool check_syntax(char* arr);
+
+bool check_syntax_2(char* arr,int size);
+
+bool check_syntax_3(char* arr,int size);
 
 float reading_elements(float elements, char* arr, int count_digits, int size);
